e9ca9d752ca56a137c3e093a7ed9a6c307267b27
mabq <alejandro.banderas@me.com>
2025-07-02 12:36:40 -0500

    all

--- system/macbook-pro-2010/hardware-configuration.nix
+++ system/macbook-pro-2010/hardware-configuration.nix
@@ -17,7 +17,8 @@
   ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" "wl" ];
-  boot.extraModulePackages = [ config.boot.kernelPackages.broadcom_sta ];
+  #boot.extraModulePackages = [ config.boot.kernelPackages.broadcom_sta ];
+  boot.extraModulePackages = [ ];
 
   fileSystems."/" = {
     device = "/dev/disk/by-uuid/e186a627-c7da-4b8b-858f-fd0000bae4ef";
