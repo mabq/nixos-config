dc398c1c6bf3be578a3717d2ba665af8f4cd356a
Alejandro Banderas <alejandro.banderas@me.com>
2026-03-12 10:52:21 -0500

    Add 'networkmanager' group to user

--- users/mabq/nixos.nix
+++ users/mabq/nixos.nix
@@ -9,8 +9,8 @@
     home = "/home/${user}";
     shell = pkgs.zsh;
     # wheel - no password for sudo actions
-    # TODO: Add `networkmanager`?
-    extraGroups = [ "wheel" ];
+    # networkmanager - required to activate wifi connections via `nmtui`
+    extraGroups = [ "wheel" "networkmanager" ];
     # `mkpasswd -m sha-512` - to create a password hash
     hashedPassword = "$6$slFKhHBtWmrAa8NN$dZD4TelNDAISrLJHAM.35K31m/0MszqHJ.7kuLdNC444FwprmHxvgU3SAcIgIeDpCFhO2EfWbU43JPnSrLGA01";
     openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC5nMLQAi4YVaKO1vQoszgy03XlgbmMAuN3wzlFHain8 alejandro.banderas@me.com" ];
