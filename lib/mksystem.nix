6f708d016cb4851da3db7e0991e1565818655c20
Alejandro Banderas <alejandro.banderas@me.com>
2026-03-12 10:38:40 -0500

    Bug fix

--- lib/mksystem.nix
+++ lib/mksystem.nix
@@ -7,16 +7,11 @@ let
   userConfig = ../users/${user}/nixos.nix;
   # homeManagerConfig = ../users/${user}/home-manager.nix;
 in nixpkgs.lib.nixosSystem {
+  specialArgs = { inherit inputs machine user; };
   modules = [
     # { nixpkgs.overlays = overlays; }
 
     machineConfig
     userConfig
-
-    # Expose some extra arguments to all modules
-    # See https://nixos-and-flakes.thiscute.world/nixos-with-flakes/nixos-flake-and-module-system#pass-non-default-parameters-to-submodules
-    {
-      _module.args = { inherit inputs machine user; };
-    }
   ];
 }
