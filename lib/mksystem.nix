74408ee786ca4eec6e32098e4420e619e08cac4d
Alejandro Banderas <alejandro.banderas@me.com>
2026-03-12 10:31:16 -0500

    Bug fix

--- lib/mksystem.nix
+++ lib/mksystem.nix
@@ -6,19 +6,17 @@ let
   machineConfig = ../machines/${machine}.nix;
   userConfig = ../users/${user}/nixos.nix;
   # homeManagerConfig = ../users/${user}/home-manager.nix;
-in {
-  nixpkgs.lib.nixosSystem = {
-    modules = [
-      # { nixpkgs.overlays = overlays; }
+in nixpkgs.lib.nixosSystem = {
+  modules = [
+    # { nixpkgs.overlays = overlays; }
 
-      machineConfig
-      userConfig
+    machineConfig
+    userConfig
 
-      # Expose some extra arguments to all modules
-      # See https://nixos-and-flakes.thiscute.world/nixos-with-flakes/nixos-flake-and-module-system#pass-non-default-parameters-to-submodules
-      {
-        _module.args = { inherit inputs machine user; };
-      }
-    ];
-  }
+    # Expose some extra arguments to all modules
+    # See https://nixos-and-flakes.thiscute.world/nixos-with-flakes/nixos-flake-and-module-system#pass-non-default-parameters-to-submodules
+    {
+      _module.args = { inherit inputs machine user; };
+    }
+  ];
 }
