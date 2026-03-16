4bfe7501b40e615c4c7f9d0b6d89c4eb1a2ba7d5
Alejandro Banderas <alejandro.banderas@me.com>
2026-03-15 19:47:37 -0500

    Replaced NetworkManager with networkd/resolved/iwd/impala

--- machines/defaults.nix
+++ machines/defaults.nix
@@ -14,14 +14,10 @@
     git
     neovim
     yazi
+    impala # TUI for managing wifi
   ];
 
   # i18n.defaultLocale = lib.mkDefault "en_US.UTF-8";
-
-  networking.networkmanager.enable = lib.mkDefault true;
-
-  networking.hostName = lib.mkDefault "${machine}";
-
   # Use the latest version of the `nix` CLI
   nix.package = lib.mkDefault pkgs.nixVersions.latest; # (2)
 
@@ -37,7 +33,7 @@
   # Enable the OpenSSH daemon
   services.openssh.enable = lib.mkDefault true;
   services.openssh.settings.PasswordAuthentication = lib.mkDefault true;
-  services.openssh.settings.PermitRootLogin = lib.mkDefault "yes"; # !!!!!!!!!!!!!!!!!!!!!!
+  services.openssh.settings.PermitRootLogin = lib.mkDefault "no";
 
   services.pipewire.enable = lib.mkDefault true;
   services.pipewire.pulse.enable = lib.mkDefault true;
@@ -51,6 +47,137 @@
 
   # Virtualization settings
   virtualisation.docker.enable = lib.mkDefault true;
+
+  # ----------------------------------------------------------------------------
+
+  # --- Network DNS
+  #     Use systemd-resolved to resolve DNS queries
+  services.resolved = {
+    enable = true;
+    settings = {
+      # Read `man 5 resolved.conf` for configuration files and options
+      # These options afffect `/etc/systemd/resolved.conf`
+      # Use `resolvectl <TAB>` to see all subcommands
+      Resolve = {
+        DNS = [
+          # Use Cloudflare DNS servers
+          "1.1.1.1" # IPv4
+          "1.0.0.1" # IPv4 fallback
+          "2606:4700:4700::1111" # IPv6
+          "2606:4700:4700::1001" # IPv6 fallback
+        ];
+        FallbackDNS = [
+          # Use Google DNS servers as fallback
+          "8.8.8.8" # IPv4
+          "8.8.4.4" # IPv4 fallback
+          "2001:4860:4860::8888" # IPv6
+          "2001:4860:4860::8844" # IPv6 fallback
+        ];
+        Domains = [ "~." ]; # use resolved for all domains
+        DNSSEC = true; # DNS Security Extensions (cryptographic signing of DNS records to verify authenticity and integrity, preventing DNS spoofing, cache poisoning, and man-in-the-middle attacks) - fully supported by Cloudflare DNS servers.
+        DNSOverTLS = true; # Encrypt DNS queries between your device and the DNS resolver using TLS (port 853), preventing eavesdropping, tampering, and ISP snooping - fully supported by Cloudflare DNS servers
+      };
+    };
+  };
+
+  # Networkd handles DHCP now (must be turned off, is on by default)
+  networking.useDHCP = lib.mkDefault false;
+
+  # Enable systemd-networkd
+  systemd.network = {
+    enable = true;
+    networks = {
+      # The key is the .network filename in `/etc/systemd/network`; match order matters (lower = higher priority)
+      "10-ethernet" = {
+        matchConfig = {
+          Name = "en* eth*"; # matches enp3s0, eno1, etc.
+        };
+        linkConfig = {
+          RequiredForOnline = "routable";
+        };
+        networkConfig = {
+          DHCP = "yes";
+          # MulticastDNS = "yes";
+        };
+        dhcpV4Config = {
+          RouteMetric = 100; # prefer wired over wireless
+          UseDNS = false;
+          UseDomains = false;
+        };
+        dhcpV6Config = {
+          RouteMetric = 100; # prefer wired over wireless
+          UseDNS = false;
+          UseDomains = false;
+        };
+      };
+      # "10-ethernet-static" = {
+      #   matchConfig = {
+      #     Name = "en* eth*"; # matches enp3s0, eno1, etc.
+      #   };
+      #   networkConfig = {
+      #     Address = "192.168.1.50/24";
+      #     Gateway = "192.168.1.1";
+      #   };
+      # };
+      "20-wlan" = {
+        matchConfig = {
+          Name = "wl*";
+        };
+        linkConfig = {
+          RequiredForOnline = "routable";
+        };
+        networkConfig = {
+          DHCP = "yes";
+          # MulticastDNS = "yes";
+        };
+        dhcpV4Config = {
+          RouteMetric = 600;
+          UseDNS = false;
+          UseDomains = false;
+        };
+        dhcpV6Config = {
+          RouteMetric = 600;
+          UseDNS = false;
+          UseDomains = false;
+        };
+      };
+      "30-wwan" = {
+        matchConfig = {
+          Name = "ww*";
+        };
+        linkConfig = {
+          RequiredForOnline = "routable";
+        };
+        networkConfig = {
+          DHCP = "yes";
+        };
+        dhcpV4Config = {
+          RouteMetric = 700;
+          UseDNS = false;
+          UseDomains = false;
+        };
+        dhcpV6Config = {
+          RouteMetric = 700;
+          UseDNS = false;
+          UseDomains = false;
+        };
+      };
+    };
+  };
+
+  # Enable iwd (Internet wireless daemon)
+  # Tools like systemd-networkd only configure IP networking (DHCP, addresses, routes). They do not handle Wi-Fi authentication.
+  # Required by impala for wifi authentication
+  networking.wireless.iwd = {
+    enable = true;
+    settings = {
+      General.EnableNetworkConfiguration = false;  # let networkd handle IPs
+      Network.EnableIPv6 = true;
+    };
+  }; 
+
+  networking.hostName = lib.mkDefault "${machine}";
+
 }
 
 # -----------------------------------------------------------------------------
--- users/mabq/nixos.nix
+++ users/mabq/nixos.nix
@@ -8,7 +8,8 @@
     isNormalUser = true;
     home = "/home/${user}";
     shell = pkgs.zsh;
-    extraGroups = [ "wheel" "networkmanager" "docker" ]; # (1)
+    # extraGroups = [ "wheel" "networkmanager" "docker" ]; # (1)
+    extraGroups = [ "wheel" "docker" ]; # (1)
     # TODO: passwd is taking precedence over this!
     hashedPassword = "$6$slFKhHBtWmrAa8NN$dZD4TelNDAISrLJHAM.35K31m/0MszqHJ.7kuLdNC444FwprmHxvgU3SAcIgIeDpCFhO2EfWbU43JPnSrLGA01"; # (2)
     openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC5nMLQAi4YVaKO1vQoszgy03XlgbmMAuN3wzlFHain8 alejandro.banderas@me.com" ];
