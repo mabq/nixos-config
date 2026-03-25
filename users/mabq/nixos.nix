45fa3e8566a6621c2a825bf2b245a3b872005b74
Alejandro Banderas <alejandro.banderas@me.com>
2026-03-24 21:52:12 -0500

    Change ssh public key

--- users/mabq/nixos.nix
+++ users/mabq/nixos.nix
@@ -13,10 +13,10 @@
     extraGroups = [ "wheel" ];
 
     # Use `mkpasswd -m sha-512` to create a passwork hash.
-    hashedPassword = "$6$slFKhHBtWmrAa8NN$dZD4TelNDAISrLJHAM.35K31m/0MszqHJ.7kuLdNC444FwprmHxvgU3SAcIgIeDpCFhO2EfWbU43JPnSrLGA01"; # [2]
+    hashedPassword = "$6$slFKhHBtWmrAa8NN$dZD4TelNDAISrLJHAM.35K31m/0MszqHJ.7kuLdNC444FwprmHxvgU3SAcIgIeDpCFhO2EfWbU43JPnSrLGA01";
 
     # No need to check whether the service is enabled, if it is not the file exist without being used.
-    openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC5nMLQAi4YVaKO1vQoszgy03XlgbmMAuN3wzlFHain8 alejandro.banderas@me.com" ]; # [3]
+    openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINjOlPls0gNkjBTOvXIbmm7HbSUOHM+erfwE4tdNVMLn" ];
   };
 }
 
