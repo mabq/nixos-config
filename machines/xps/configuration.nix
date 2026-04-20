e3ca7cdb106c404bc5af3388847232605e8b62b0
Alejandro Banderas <alejandro.banderas@me.com>
2026-04-20 13:55:23 -0500

    No facter DHCP for xps

--- machines/xps/configuration.nix
+++ machines/xps/configuration.nix
@@ -5,16 +5,9 @@
     ../../modules/diskConfig/single-disk-ext4-crypt.nix
   ];
 
-  # boot.loader.grub.enable = true;
-  # boot.loader.grub.efiSupport = true;
-  # boot.loader.grub.efiInstallAsRemovable = true;
-
   disko.devices.disk.main.device = "/dev/sda";
 
-  # This relative path refers to the directory on your source machine (the computer where you run the nixos-anywhere command).
-  # The facter.json file is never "copied" as a loose file to the target machine. Instead, it is baked directly into the Nix Store on the target.
+  # What facter configures? -> https://github.com/NixOS/nixpkgs/blob/nixos-unstable/nixos/modules/hardware/facter/facter.md#what-gets-configured-module-hardware-facter-features
   hardware.facter.reportPath = ./facter.json;
-  systemd.network.wait-online.enable = false;
-
-  system.stateVersion = "25.11";
+  hardware.facter.detected.dhcp.enable = false;
 }
