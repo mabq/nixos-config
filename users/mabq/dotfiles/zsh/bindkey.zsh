38311c61a455426e3bda6024496cef9b178dda5b
Alejandro Banderas <alejandro.banderas@me.com>
2026-04-25 23:41:37 -0500

    zsh + delete whole word on Ctrl+Backspace

--- users/mabq/dotfiles/zsh/bindkey.zsh
+++ users/mabq/dotfiles/zsh/bindkey.zsh
@@ -5,6 +5,10 @@ bindkey -v
 bindkey '^[[1;5C' forward-word
 bindkey '^[[1;5D' backward-word
 
+# Control + Backspace to delete a word backward
+bindkey '^H' backward-kill-word # common sequence for Ctrl+Backspace
+bindkey '^[[3;5~' backward-kill-word # alternative sequence used by some terminals
+
 # Zsh bug fix (https://github.com/spaceship-prompt/spaceship-prompt/issues/91#issuecomment-327996599)
 bindkey '^?' backward-delete-char
 
