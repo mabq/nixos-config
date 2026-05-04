ae50630e98d1a5f5eb118a1c90d9561ad01c51f7
Alejandro Banderas <alejandro.banderas@me.com>
2026-05-04 00:38:57 -0500

    keyd move configs

--- modules/keyd.nix
+++ modules/keyd.nix
@@ -3,6 +3,7 @@
   lib,
   pkgs,
   user,
+  profile,
   ...
 }:
 with lib; {
@@ -13,7 +14,7 @@ with lib; {
       pkgs.keyd # Key remapping daemon for Linux (required to have the `keyd` command)
     ];
 
-    etc."keyd".source = ../config/keyd; # this a nixos module, we don't have the repo path here to create a outOfStore link.
+    etc."keyd".source = ../users/${user}/${profile}/config/keyd; # this a nixos module, we don't have the repo path here to create a outOfStore link.
   };
 
   users.users.${user}.extraGroups = ["keyd"];
