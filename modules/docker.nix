dc88169a5b62c72f3f9f68c2c4e0ad286bc01632
Alejandro Banderas <alejandro.banderas@me.com>
2026-05-04 00:23:05 -0500

    docker

--- modules/docker.nix
+++ modules/docker.nix
@@ -1,9 +1,13 @@
 {
   lib,
+  pkgs,
   user,
   ...
 }:
 with lib; {
+  environment.systemPackages = [
+    pkgs.lazydocker # Simple terminal UI for both docker and docker-compose
+  ];
   virtualisation.docker.enable = mkDefault true;
   users.users.${user}.extraGroups = ["docker"];
 }
