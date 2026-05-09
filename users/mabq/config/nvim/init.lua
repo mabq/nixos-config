5d297f5b9f2e7c798c27378d3b0f1b1486dc1cec
mabq <alejandro.banderas@me.com>
2026-05-08 23:45:53 -0500

    nvim lsp configs

--- users/mabq/config/nvim/init.lua
+++ users/mabq/config/nvim/init.lua
@@ -2,8 +2,34 @@ require("config.options")
 
 -- Plugins
 vim.pack.add{
-  { src = 'https://github.com/neovim/nvim-lspconfig' }, -- Default LSP configurations for Neovim
+  { src = 'https://github.com/nvim-lua/plenary.nvim' }, -- Plenary, required by many plugins
+  { src = 'https://github.com/neovim/nvim-lspconfig' }, -- https://github.com/neovim/nvim-lspconfig#quickstart
+  { src = 'https://github.com/kdheepak/lazygit.nvim' }, -- Lazygit
 }
 
 -- LSP servers are installed via Mason or Nix
 vim.lsp.enable('nixd')
+vim.lsp.enable('lua_ls')
+
+-- vim.lsp.config('lua_ls', {
+--   settings = {
+--     Lua = {
+--       runtime = 'LuaJIT',
+--     },
+--     diagnostics = {
+--       globals = {
+--         'vim',
+--         'require'
+--       }
+--     },
+--     workspace = {
+--       library = vim.api.nvim_get_runtime_file("", true),
+--     },
+--     telemetry = {
+--       enable = false,
+--     },
+--   },
+-- })
+
+vim.keymap.set('n', '<leader>gg', ':LazyGit<CR>', { desc = 'Open LazyGit' })
+
