1dc92789470c5142a934c8f5b7fc50badd7dc68e
Alejandro Banderas <alejandro.banderas@me.com>
2026-05-15 23:44:49 -0500

    nvim telescope

--- users/mabq/config/nvim/lua/custom/plugins/telescope.lua
+++ users/mabq/config/nvim/lua/custom/plugins/telescope.lua
@@ -41,9 +41,15 @@ return {
 				},
 				sorting_strategy = "ascending",
 			},
+			pickers = {
+				grep_string = {
+					use_regex = true, -- enable regex
+				},
+			},
 			extensions = {
 				fzf = {
 					-- https://github.com/nvim-telescope/telescope-fzf-native.nvim?tab=readme-ov-file#telescope-setup-and-configuration
+					fuzzy = false, -- exact matches by default
 				},
 				["ui-select"] = {
 					require("telescope.themes").get_dropdown({}),
@@ -56,14 +62,16 @@ return {
 
 		local builtin = require("telescope.builtin")
 
-		vim.keymap.set("n", "<leader><leader>", builtin.git_files, { desc = "[] Search Git Files" })
-		vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [P]roject files" })
-		vim.keymap.set("n", "<leader>/", builtin.live_grep, { desc = "[S]earch by [G]rep" })
-		vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch [W]ord" })
-		vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
-		vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
-		vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
-		vim.keymap.set("n", "<leader>st", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
-		vim.keymap.set("n", "<leader>ss", builtin.resume, { desc = "[S]earch [R]esume" })
+		vim.keymap.set("n", "<leader><leader>", builtin.resume, { desc = "Search Resume" })
+		vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "Search Files" })
+		vim.keymap.set("n", "<leader>sg", builtin.git_files, { desc = "Search Git Files" })
+		vim.keymap.set("n", "<leader>sl", builtin.live_grep, { desc = "Search Live" })
+		vim.keymap.set("n", "<leader>ss", function()
+			builtin.grep_string({ search = vim.fn.input("Search: ") })
+		end, { desc = "Search String" }) -- big projects
+		vim.keymap.set("n", "<leader>st", builtin.builtin, { desc = "Search Telescope" })
+		vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "Search Diagnostics" })
+		vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "Search Help" })
+		vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "Search Keymaps" })
 	end,
 }
