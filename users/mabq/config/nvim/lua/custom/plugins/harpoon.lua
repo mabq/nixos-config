89940b29659dad2f9dbd50f625db21cf1fcc57bf
Alejandro Banderas <alejandro.banderas@me.com>
2026-05-16 00:13:14 -0500

    nvim harpoon

--- users/mabq/config/nvim/lua/custom/plugins/harpoon.lua
+++ users/mabq/config/nvim/lua/custom/plugins/harpoon.lua
@@ -1,47 +1,47 @@
 return {
-	"ThePrimeagen/harpoon",
-	branch = "harpoon2",
-	dependencies = {
-		{ "nvim-lua/plenary.nvim", lazy = true },
-	},
-	config = function()
-		local harpoon = require("harpoon")
-		harpoon:setup({
-			settings = { save_on_toggle = true },
-		})
+  "ThePrimeagen/harpoon",
+  branch = "harpoon2",
+  dependencies = {
+    { "nvim-lua/plenary.nvim", lazy = true },
+  },
+  config = function()
+    local harpoon = require "harpoon"
+    harpoon:setup {
+      settings = { save_on_toggle = true },
+    }
 
-		-- Highlight current file in menu
-		local harpoon_extensions = require("harpoon.extensions")
-		harpoon:extend(harpoon_extensions.builtins.highlight_current_file())
+    -- Highlight current file in menu
+    local harpoon_extensions = require "harpoon.extensions"
+    harpoon:extend(harpoon_extensions.builtins.highlight_current_file())
 
-		-- Add to/Open harppon list
-		vim.keymap.set("n", "<C-space>", function()
-			harpoon.ui:toggle_quick_menu(harpoon:list())
-		end)
-		vim.keymap.set("n", "<C-a>", function()
-			harpoon:list():add()
-		end)
+    -- Add to/Open harppon list
+    vim.keymap.set("n", "<leader>hh", function()
+      harpoon.ui:toggle_quick_menu(harpoon:list())
+    end)
+    vim.keymap.set("n", "<leader>ha", function()
+      harpoon:list():add()
+    end)
 
-		-- -- Quickly move through the main 4 harppon files
-		-- vim.keymap.set("n", "<left>", function()
-		-- 	harpoon:list():select(1)
-		-- end)
-		-- vim.keymap.set("n", "<down>", function()
-		-- 	harpoon:list():select(2)
-		-- end)
-		-- vim.keymap.set("n", "<up>", function()
-		-- 	harpoon:list():select(3)
-		-- end)
-		-- vim.keymap.set("n", "<right>", function()
-		-- 	harpoon:list():select(4)
-		-- end)
+    -- -- Quickly move through the main 4 harppon files
+    -- vim.keymap.set("n", "<left>", function()
+    -- 	harpoon:list():select(1)
+    -- end)
+    -- vim.keymap.set("n", "<down>", function()
+    -- 	harpoon:list():select(2)
+    -- end)
+    -- vim.keymap.set("n", "<up>", function()
+    -- 	harpoon:list():select(3)
+    -- end)
+    -- vim.keymap.set("n", "<right>", function()
+    -- 	harpoon:list():select(4)
+    -- end)
 
-		-- Toggle previous & next buffers stored within Harpoon list
-		vim.keymap.set("n", "<C-n>", function()
-			harpoon:list():next()
-		end)
-		vim.keymap.set("n", "<C-p>", function()
-			harpoon:list():prev()
-		end)
-	end,
+    -- Toggle previous & next buffers stored within Harpoon list
+    vim.keymap.set("n", "<up>", function()
+      harpoon:list():prev()
+    end)
+    vim.keymap.set("n", "<down>", function()
+      harpoon:list():next()
+    end)
+  end,
 }
