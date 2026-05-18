d8f9cc930b5011efada758b2d60fd97ffb9b41d7
Alejandro Banderas <alejandro.banderas@me.com>
2026-05-18 14:28:56 -0500

    nvim devicons

---
+++ users/mabq/config/nvim/lua/plugins/devicons.lua
@@ -0,0 +1,19 @@
+-- IMPORTANT: A nerd-font is required
+
+return {
+  "nvim-tree/nvim-web-devicons",
+  opts = true,
+}
+
+--[[
+
+A Nerd Font gives your terminal the ability to render icon characters.
+
+This plugin tells Neovim:
+
+  Which icon belongs to each filetype, e.g. `.js`
+  Which color to use for each icon.
+  Which highlight group to use.
+  What to display for folders vs symlinks vs gitignored files
+
+--]]
