f6f44296b513c575158271a38df0a810a3310a6d
mabq <alejandro.banderas@me.com>
2026-07-30 23:04:01 -0500

    all

--- users/mabq/default.nix
+++ users/mabq/default.nix
@@ -47,7 +47,7 @@
       };
       "ssh_private_key" = {
         # Create the file with the right permissions
-        # path = "/home/${user}/.ssh/id_ed25519";
+        path = "/home/${user}/.ssh/id_ed25519";
         mode = "0600";
         owner = "${user}";
         group = "users";
@@ -69,19 +69,4 @@
     };
   };
 
-  home-manager.users.${user} =
-    { config, ... }:
-    let
-      mkOutOfStoreSymlink = config.lib.file.mkOutOfStoreSymlink;
-    in
-    {
-      home = {
-        file = {
-          ".ssh/id_ed25519" = {
-            source = mkOutOfStoreSymlink config.sops.secrets."ssh_private_key".path;
-            force = true;
-          };
-        };
-      };
-    };
 }
