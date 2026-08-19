b890b3958a1e5984b49e92a684c8656f71bd7cc0
mabq <alejandro.banderas@me.com>
2026-08-18 22:39:25 -0500

    all

--- modules/programs/foot.nix
+++ modules/programs/foot.nix
@@ -13,8 +13,6 @@
 
         file = {
           ".config/foot/foot.ini" = {
-            # Unfortunately, foot does not allow variables in paths given to
-            # `include`, so we must use a user's scoped file.
             source = mkOutOfStoreSymlink "${repoDir}/config/foot.ini";
             force = true;
           };
--- profiles/plex-server.nix
+++ profiles/plex-server.nix
@@ -2,7 +2,6 @@
   config,
   user,
   pkgs,
-  repoDir,
   ...
 }:
 {
@@ -14,49 +13,30 @@
     ../modules/programs/git.nix
     ../modules/programs/keyd.nix
     ../modules/programs/neovim.nix
+
+    ../modules/programs/foot
   ];
 
-  services = {
-    tailscale = {
-      # This key must be opened by the user's module.
-      authKeyFile = config.sops.secrets."mabqTailnet_sharedTagKey".path;
-      # For possible flags see https://tailscale.com/docs/reference/tailscale-cli#set
-      extraSetFlags = [ "--ssh" ];
-    };
+  services.tailscale = {
+    # This key must be opened by the user's module.
+    authKeyFile = config.sops.secrets."mabqTailnet_sharedTagKey".path;
+    # For possible flags see https://tailscale.com/docs/reference/tailscale-cli#set
+    extraSetFlags = [ "--ssh" ];
+  };
 
-    plex = {
-      # Configure Plex via `http://<SERVER-IP>:32400/web`
-      enable = true;
-      openFirewall = true;
-      user = "${user}"; # ⚠️ should not run as my user, it could read secret files only readble by me
-    };
+  services.plex = {
+    # Configure Plex via `http://<SERVER-IP>:32400/web`
+    enable = true;
+    openFirewall = true;
+    user = "${user}"; # ⚠️ should not run as my user, it could read secret files only readble by me
   };
 
   programs.niri.enable = true;
 
-  home-manager.users.${user} =
-    { pkgs, config, ... }:
-    let
-      mkOutOfStoreSymlink = config.lib.file.mkOutOfStoreSymlink;
-    in
-    {
-      home = {
-        packages = with pkgs; [
-          brave
-          fuzzel
-          foot
-        ];
-        file = {
-          ".config/foot/foot.ini" = {
-            source = mkOutOfStoreSymlink "${repoDir}/config/foot/foot.ini";
-            force = true;
-          };
-        };
-      };
-    };
-
-  environment.sessionVariables = {
-    # Required to run brave in wayland mode
-    NIXOS_OZONE_WL = "1";
+  home-manager.users.${user} = {
+    home.packages = with pkgs; [
+      brave
+      fuzzel
+    ];
   };
 }
