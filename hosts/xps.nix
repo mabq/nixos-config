7ae819f23b1b5109c942d22fcc9db2cf1431c337
mabq <alejandro.banderas@me.com>
2026-08-29 23:22:30 -0500

    xps

--- hosts/xps.nix
+++ hosts/xps.nix
@@ -4,31 +4,34 @@
   disk,
   ...
 }:
+
+# Manual installations --------------------------------------------------------
+
 {
   imports = [
-    # Manual install
-    # --------------
-    # ./hardware-configuration/xps-20260724.nix
-
-    # Nixos-anywhere
-    # --------------
-    inputs.disko.nixosModules.disko
-    ./disko/ext4-encrypted.nix
-    # ./hardware-configuration/xps-20260729.nix
+    ./hardware-configuration/xps-20260724.nix
   ];
 
   boot.loader.grub.enable = true;
   boot.loader.grub.device = disk;
-
-  # Nixos-anywhere
-  # --------------
-  disko.devices.disk.main.device = disk;
-  hardware.facter.reportPath = ./facter/xps-20260829.json;
 }
 
-/*
-  Disko configurations are used to partition the disk during installation and
-  also to mount filessystems.
+# Nixos-anywhere --------------------------------------------------------------
 
-  See [Prepare Hardware Configuration](https://nix-community.github.io/nixos-anywhere/quickstart.html#8-prepare-hardware-configuration)
-*/
+# {
+#   imports = [
+#     # inputs.disko.nixosModules.disko
+#     # ./disko/ext4-encrypted.nix
+
+#     # If using `nixos-generate-config`
+#     # ./hardware-configuration/xps-20260729.nix
+#   ];
+#
+#   boot.loader.grub.enable = true;
+#   boot.loader.grub.device = disk;
+#
+#   disko.devices.disk.main.device = disk;
+
+#   # If using `nixos-facter`
+#   hardware.facter.reportPath = ./facter/xps-20260829.json;
+# }
