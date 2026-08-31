c935658685da6c819f5a3b28b5e282783c16ddf0
mabq <alejandro.banderas@me.com>
2026-08-30 22:25:49 -0500

    facter xps

--- hosts/xps/default.nix
+++ hosts/xps/default.nix
@@ -1,32 +1,31 @@
 {
-  # inputs,
+  inputs,
   disk,
-  # host,
   ...
 }:
 
 # Manual installations --------------------------------------------------------
 
-{
-  imports = [
-    ./hardware-configuration.nix
-  ];
-
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = disk;
-}
-
-# Nixos-anywhere --------------------------------------------------------------
-
 # {
 #   imports = [
-#     inputs.disko.nixosModules.disko
-#     ../../modules/disko/ext4-encrypted.nix
+#     ./hardware-configuration.nix
 #   ];
-#
-#   disko.devices.disk.main.device = disk; # https://nix-community.github.io/nixos-anywhere/quickstart.html#4-configure-storage
-#   hardware.facter.reportPath = ./facter.json; # https://nix-community.github.io/nixos-anywhere/quickstart.html#81-nixos-facter
 #
 #   boot.loader.grub.enable = true;
 #   boot.loader.grub.device = disk;
 # }
+
+# Nixos-anywhere --------------------------------------------------------------
+
+{
+  imports = [
+    inputs.disko.nixosModules.disko
+    ../../modules/disko/ext4-encrypted.nix
+  ];
+
+  disko.devices.disk.main.device = disk; # https://nix-community.github.io/nixos-anywhere/quickstart.html#4-configure-storage
+  hardware.facter.reportPath = ./facter.json; # https://nix-community.github.io/nixos-anywhere/quickstart.html#81-nixos-facter
+
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = disk;
+}
