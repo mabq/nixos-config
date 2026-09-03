5f5ec823f5d8445d4f2f9b1ed18e699b9652d69f
mabq <alejandro.banderas@me.com>
2026-09-03 12:15:58 -0500

    onChange script

--- profiles/modules/bat.nix
+++ profiles/modules/bat.nix
@@ -1,37 +1,50 @@
 {
-  configName,
+  configName ? "default",
 }:
 {
+  pkgs,
   user,
   repoDir,
-  currentThemeDir,
+  theme,
+  # currentThemeDir,
   ...
 }:
 {
   home-manager.users.${user} =
-    { config, ... }:
+    { config, lib, ... }:
     let
       mkOutOfStoreSymlink = config.lib.file.mkOutOfStoreSymlink;
     in
     {
-      # By enabling the program, instead of just adding the pkgs, home-manager
-      # runs an activation script that rebuilds the bat cache, which is
-      # required for a new theme to actually apply.
-      #  https://github.com/sharkdp/bat#adding-new-themes
-      #  https://github.com/nix-community/home-manager/blob/d9d750e4fc11c10cab2da677bdd31e427f3a3a71/modules/programs/bat.nix#L210
-      programs.bat.enable = true;
-
       home = {
+        packages = with pkgs; [
+          bat # Cat clone with syntax highlighting and Git integration
+        ];
+
         file = {
           ".config/bat/config" = {
             source = mkOutOfStoreSymlink "${repoDir}/config/bat/${configName}";
             force = true;
           };
+          # ".config/bat/themes/current.tmTheme" = {
+          #   source = mkOutOfStoreSymlink "${currentThemeDir}/bat.tmTheme";
+          #   force = true;
+          # };
           ".config/bat/themes/current.tmTheme" = {
-            source = mkOutOfStoreSymlink "${currentThemeDir}/bat.tmTheme";
+            source = mkOutOfStoreSymlink "${repoDir}/themes/${theme}/bat.tmTheme";
             force = true;
+            onChange = ''
+              ${pkgs.bat}/bin/bat cache --build
+            '';
           };
         };
+
+        # Bat requires a cache build to update the theme
+        # activation = {
+        #   batCache = lib.hm.dag.entryAfter [ "linkGeneration" ] ''
+        #     ${pkgs.bat}/bin/bat cache --build
+        #   '';
+        # };
       };
     };
 }
