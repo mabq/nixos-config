6e494bac989c5b2502aa56c03dbca940342e0c6b
mabq <alejandro.banderas@me.com>
2026-09-04 17:28:19 -0500

    all

--- profiles/hardware/keyd.nix
+++ profiles/hardware/keyd.nix
@@ -15,7 +15,7 @@ let
   # cannot use `mkOutOfStoreSymlink` (home-manager) with an absolute path.
   # We can derive an absolute path from the flake root to avoid breaking the
   # path if we move this module.
-  _repoConfigDir = self + (lib.strings.removePrefix "${repoDir}" "${repoConfigDir}");
+  # _repoConfigDir = self + (lib.strings.removePrefix "${repoDir}" "${repoConfigDir}");
 in
 {
   # Keyd is a system-level service
@@ -30,6 +30,7 @@ in
       pkgs.keyd # Key remapping daemon for Linux
     ];
 
-    etc."keyd".source = "${_repoConfigDir}/keyd/${configName}";
+    # etc."keyd".source = "${_repoConfigDir}/keyd/${configName}";
+    etc."keyd".source = "../../config/keyd/${configName}";
   };
 }
